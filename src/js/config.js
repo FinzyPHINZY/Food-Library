ef72bdb6fb8e2fb4eb015837ddf9c6a141eff3ce
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-02-01 21:11:25 +0100

    added modules and files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '03a2d6e4-674d-4429-84bd-42691abc2e71';
+export const MODAL_CLOSE_SEC = 2.5;
